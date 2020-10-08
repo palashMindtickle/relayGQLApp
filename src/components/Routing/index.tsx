6f59a57d7b37d9c26fdabf82458c0cdd2e152137
palashMindtickle <palash.chordia@mindtickle.com>
2020-10-08 15:48:37 +0530

    chore: update

--- src/components/Routing/index.tsx
+++ src/components/Routing/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Routes from './routes'
 import RoutesList from '../RoutesList';
 import AllCountries from '../../modules/AllCountries';
--- src/components/root.tsx
+++ src/components/root.tsx
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import { Link } from 'react-router-dom';
 
 const history = createBrowserHistory();
 
