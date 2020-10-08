70b69b03c2945143d1f5beb63ac8b91b69363f69
palashMindtickle <palash.chordia@mindtickle.com>
2020-10-08 15:46:19 +0530

    chore: updated

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,24 @@
-module.exports = require('@mindtickle/frontend-configs/src/eslint');
+module.exports = {
+  extends: ['react-app'],
+  // plugins: ['prettier'],
+  rules: {
+    // 'prettier/prettier': 'error',
+    'arrow-body-style': ['error', 'as-needed'],
+    'no-unused-expressions': 'off',
+    'import/no-extraneous-dependencies': 0,
+    'no-console': 0,
+    complexity: ['error', 2],
+    'max-depth': ['error', { max: 2 }],
+    'max-params': ['error', 3],
+    'max-statements': ['error', 6],
+    // 'no-unused-vars': 'error',
+    'no-unused-labels': 'error',
+    //https://stackoverflow.com/questions/59265981/typescript-eslint-missing-file-extension-ts-import-extensions
+    'import/extensions': [
+      0
+    ],
+    'prettier/prettier': 0,
+    'prettier': 0,
+    'import/first': 0
+  },
+};
